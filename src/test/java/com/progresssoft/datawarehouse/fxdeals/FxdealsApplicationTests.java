1df9474c26c085941918335aa3db5e0e18991b81
Farouq Gfishi <farouqgfishi@gmail.com>
2024-07-03 21:45:32 +0300

    feat: add testing feature to the fields

--- src/test/java/com/progresssoft/datawarehouse/fxdeals/FxdealsApplicationTests.java
+++ src/test/java/com/progresssoft/datawarehouse/fxdeals/FxdealsApplicationTests.java
@@ -9,4 +9,74 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FxdealsApplicationTests {
 
+	private FXDeal fxDeal;
+
+	@BeforeEach
+	public void setUp() {
+		fxDeal = new FXDeal();
+	}
+
+	@Test
+	public void testFromCurrencyNotNull() {
+		Assertions.assertNull(fxDeal.getFromCurrency());
+	}
+
+	@Test
+	public void testFromCurrencyNotEmpty() {
+		Assertions.assertNull(fxDeal.getFromCurrency());
+	}
+
+	@Test
+	public void testFromCurrencyContainsThreeLetters() {
+		fxDeal.setFromCurrency("USD");
+		Assertions.assertEquals(fxDeal.getFromCurrency().length(), 3);
+	}
+
+	@Test
+	public void testFromCurrencyNotContainsThreeLetters() {
+		fxDeal.setFromCurrency("ABCD");
+		Assertions.assertNotEquals(fxDeal.getFromCurrency().length(), 3);
+	}
+
+	@Test
+	public void testToCurrencyNotNull() {
+		Assertions.assertNull(fxDeal.getToCurrency());
+	}
+
+	@Test
+	public void testToCurrencyNotEmpty() {
+		Assertions.assertNull(fxDeal.getToCurrency());
+	}
+
+	@Test
+	public void testToCurrencyContainsThreeLetters() {
+		fxDeal.setToCurrency("USD");
+		Assertions.assertEquals(fxDeal.getToCurrency().length(), 3);
+	}
+
+	@Test
+	public void testToCurrencyNotContainsThreeLetters() {
+		fxDeal.setToCurrency("ABCD");
+		Assertions.assertNotEquals(fxDeal.getToCurrency().length(), 3);
+	}
+
+	@Test
+	public void testAmountGreaterThenZero() {
+		fxDeal.setAmountDeal(1.0);
+		Assertions.assertTrue(fxDeal.getAmountDeal() >= 0.0);
+	}
+
+	@Test
+	public void testAmountEqualZero() {
+		fxDeal.setAmountDeal(0.0);
+		Assertions.assertTrue(fxDeal.getAmountDeal() >= 0.0);
+	}
+
+	@Test
+	public void testAmountNotSmallerThanZero() {
+		fxDeal.setAmountDeal(-1.0);
+		Assertions.assertFalse(fxDeal.getAmountDeal() >= 0.0);
+	}
+
 }
+
